f1a364b8b2edc63371a82560946c6f1c74757fa0
MdaaaaO <georg.kasper08@gmail.com>
2015-10-22 11:33:53 +0200

    added exceptions

--- src/stack/MyArrayStack.java
+++ src/stack/MyArrayStack.java
@@ -1,5 +1,8 @@
 package stack;
 
+import stack.StackEmptyException;
+import stack.StackFullException;
+
 /**
  * Exercise 1.a - Stack Implementation as an Array with Exception-Handling
  * 
@@ -7,10 +10,10 @@ package stack;
  * @date 10/21/2015
  */
 public class MyArrayStack extends Stack {
-	private int array_size = 1024;
+	private int array_size = 2;
 	private Object[] intern_array = new Object[this.array_size];
 	private int intern_index = -1;
-
+	
 	@Override
 	public String version() {
 		return "1.a - Stack Implementation as an Array with Exception-Handling";
@@ -22,16 +25,24 @@ public class MyArrayStack extends Stack {
 	}
 
 	@Override
-	public void push(Object element) {
-		this.intern_index++;
-		this.intern_array[intern_index] = element;
+	public void push(Object element) throws StackFullException {
+		if (this.intern_index == this.array_size - 1) {
+			throw new StackFullException("You can't push to a full stack.");
+		} else {
+			this.intern_index++;
+			this.intern_array[intern_index] = element;
+		}
 	}
 
 	@Override
-	public Object pop() {
-		Object element = this.intern_array[this.intern_index];
-		this.intern_index--;
-		return element;
+	public Object pop() throws StackEmptyException {
+		if (this.intern_index == -1) {
+			throw new StackEmptyException("You can't pop an empty stack.");
+		} else {
+			Object element = this.intern_array[this.intern_index];
+			this.intern_index--;
+			return element;
+		}
 	}
 
 	@Override
@@ -65,14 +76,16 @@ public class MyArrayStack extends Stack {
 
 	@Override
 	public String toString() {
+		
 		String toString = new String();
-		for (int i = 0; i < this.intern_index; i++) {
-			toString += this.intern_array[i].toString() + " ";
-//			System.out.print(this.intern_array[i].toString() + " ");
+		if (this.intern_index == -1) {
+			toString = "NULL";
+		} else {
+			for (int i = 0; i < this.intern_index; i++) {
+				toString += this.intern_array[i].toString() + ", ";
+			}
+			toString += "[" + this.intern_array[this.intern_index].toString() + "]";
 		}
-		System.out.println(
-				"TOP [" + this.intern_array[this.intern_index].toString() + "]");
-		toString += "TOP [" + this.intern_array[this.intern_index].toString() + "]";
 		return toString;
 	}
 
