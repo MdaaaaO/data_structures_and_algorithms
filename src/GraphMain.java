e44583a3aac561fcb8ab9ac7f4beaec7c847e8d1
Georg Kasper <MdaaaaO>
2015-12-07 08:23:18 -0800

    added test for DFS

--- src/GraphMain.java
+++ src/GraphMain.java
@@ -6,6 +6,8 @@ public class GraphMain {
 		
 		String file = "/home/mdaaaa/projects/data_structures_and_algorithms/input/GRAPH";
 		Graph graph = new Graph(file);
-		System.out.println(graph.toString());
+//		System.out.println(graph.toString());
+//		System.out.println(graph.isEulerianGraph());
+		graph.graphDFS(1);
 	}
 }
